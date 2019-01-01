4323a8e2752343e26c6f21aeb1c9e3f0bd3db1b5
ziper02 <ziper02@gmail.com>
2019-01-01 11:46:00 +0200

    Its zivush

--- Projecton/src/Client/Hello.java
+++ Projecton/src/Client/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello GitHub");
+		System.out.println("Hello Ziv");
 	}
 
 }
