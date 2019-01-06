08aba984d2d9a44c385ae4df136530a208f1583e
ziper02 <ziper02@gmail.com>
2019-01-06 22:19:21 +0200

    Revert "fixed spelling"
    This reverts commit a538a9ce4b1010076673a698a870a34d7d193843.

--- Projecton/src/Common/User.java
+++ Projecton/src/Common/User.java
@@ -5,6 +5,6 @@ public class User implements IEntity
 	private String id; // if server returns here null -> user not exist
 					  // in the Client we will first check this atribute
 	private String password;
-	private int permission; // 1 = Library Director , 2 = Librarian , 3 = reader account
+	private int permition; // 1 = Library Director , 2 = Librarian , 3 = reader account
 	private boolean isOnline; 
 }
