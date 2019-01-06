fe9bbaa54c9bd4a59c4d2260e2d326e3b0dec780
ziper02 <ziper02@gmail.com>
2019-01-07 00:14:01 +0200

    Added all DBControllers

--- Projecton/src/Server/ABookDBController.java
+++ Projecton/src/Server/ABookDBController.java
@@ -15,18 +15,4 @@ public abstract class  ABookDBController
 {
 
 	
-	public static ObjectMessage  Selection(Connection conn,ObjectMessage objectMessage)
-	{
-		if(objectMessage.getMessage().equals("AddBook"))
-		{
-			AddBook(conn,objectMessage);
-		}
-			
-	}
-	
-	
-	private static ObjectMessage AddBook(Connection conn,ObjectMessage object)
-	{
-		Book book=(Book)object.getObjectList().get(0);
-	}
 }
---
+++ Projecton/src/Server/ACopyDBController.java
@@ -0,0 +1,5 @@
+package Server;
+
+public abstract class ACopyDBController {
+
+}
