e31c718b8dccea98839e10377c9efe1fdfcc075d
ziper02 <ziper02@gmail.com>
2019-01-06 22:28:30 +0200

    try

--- Projecton/src/Common/User.java
+++ Projecton/src/Common/User.java
@@ -4,7 +4,7 @@ public class User implements IEntity
 {
 	private String id; // if server returns here null -> user not exist
 					  // in the Client we will first check this atribute
-	private String password;
+	private String password;// user password
 	private int permission; // 1 = Library Director , 2 = Librarian , 3 = reader account
 	private boolean isOnline; 
 }
