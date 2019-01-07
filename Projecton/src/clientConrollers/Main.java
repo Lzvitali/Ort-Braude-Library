6eb1b52a65dcd10d43bc6cff06ee88b0106d4e0a
Vitali <Vitali@10.0.0.3>
2019-01-07 23:20:49 +0200

    editing the GUIs..

--- Projecton/src/clientConrollers/Main.java
+++ Projecton/src/clientConrollers/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application
 	{
 		Parent root;
 		 
-		root = FXMLLoader.load(getClass().getResource("/clientBounderies/StartPanel.fxml"));
+		root = FXMLLoader.load(getClass().getResource("/clientBounderiesReaderAccount/StartPanelReaderAccount.fxml"));
 			
 		//attach scene graph to scene
 		Scene scene = new Scene(root);
