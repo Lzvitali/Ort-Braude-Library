b9290c7fbddc31440d37ba736eab93db77a16935
Vitali <Vitali@10.0.0.3>
2019-01-11 12:09:03 +0200

    updeted OBLServer

--- Projecton/src/Server/ACopyDBController.java
+++ Projecton/src/Server/ACopyDBController.java
@@ -1,5 +1,23 @@
 package Server;
 
-public abstract class ACopyDBController {
+import java.sql.Connection;
+
+import Common.ObjectMessage;
+
+public abstract class ACopyDBController 
+{
+	
+	/**
+	 * This function sorts the request in the 'msg' to the relevant function and returns the answer
+	 * @param msg - the object from the client
+	 * @param connToSQL - the connection to the MySQL created in the Class OBLServer
+	 * @return ObjectMessage with the answer to the client
+	 */
+	public static ObjectMessage selection(ObjectMessage msg, Connection connToSQL)
+	{
+		
+		
+		return null; //TODO: delete this line
+	}
 
 }
