678f76be1e655a4bc83acc9d2c006ec76e1e5cfd
Nataliya Kamyenyeva <Nataliya Kamyenyeva@DESKTOP-MTH3PAF>
2019-01-11 12:01:42 +0200

    little changes in addBook controller

--- Projecton/src/clientBounderiesLibrarian/AddBookController.java
+++ Projecton/src/clientBounderiesLibrarian/AddBookController.java
@@ -3,6 +3,12 @@ package clientBounderiesLibrarian;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
+
+import Common.IGUIController;
+import Common.ObjectMessage;
+import clientCommonBounderies.StartPanelController;
+import clientConrollers.OBLClient;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -11,65 +17,83 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 
-public class AddBookController {
+public class AddBookController implements IGUIController
+{
+	OBLClient client;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private URL location;
 
-    @FXML
-    private URL location;
+	@FXML
+	private JFXTextField BookTitleTextField;
 
-    @FXML
-    private JFXTextField BookTitleTextField;
+	@FXML
+	private JFXTextField BookAuthorTextField;
 
-    @FXML
-    private JFXTextField BookAuthorTextField;
+	@FXML
+	private JFXTextField PublishedYearTextField;
 
-    @FXML
-    private JFXTextField PublishedYearTextField;
+	@FXML
+	private JFXTextField EditionTextField;
 
-    @FXML
-    private JFXTextField EditionTextField;
+	@FXML
+	private JFXTextField ShelfNumberTextField;
 
-    @FXML
-    private JFXTextField ShelfNumberTextField;
+	@FXML
+	private JFXCheckBox DesiredCheckBox;
 
-    @FXML
-    private JFXCheckBox DesiredCheckBox;
+	@FXML
+	private ChoiceBox<String> TopicChoiceBox;
 
-    @FXML
-    private ChoiceBox<?> TopicChoiceBox;
+	@FXML
+	private DatePicker PurchaseDateDatePicker;
 
-    @FXML
-    private DatePicker PurchaseDateDatePicker;
+	@FXML
+	private Button UploadBtn;
 
-    @FXML
-    private Button UploadBtn;
+	@FXML
+	private JFXButton CancelBtn;
 
-    @FXML
-    private JFXButton CancelBtn;
+	@FXML
+	private JFXButton SaveBtn;
 
-    @FXML
-    private JFXButton SaveBtn;
+	
+	//lo asiti
+	@FXML
+	void cancelBtnClicked(ActionEvent event) 
+	{
+		client.handleMessageFromClient(new ObjectMessage("booooom2"));
+	}
 
-    @FXML
-    void cancelBtnClicked(ActionEvent event) {
+	//lo asiti
+	@FXML
+	void saveAddNewBook(ActionEvent event) 
+	{
+		client.handleMessageFromClient(new ObjectMessage("booooom2"));
+	}
 
-    }
+	//lo asiti
+	@FXML
+	void uploadTableOfContents(ActionEvent event) 
+	{
+		client.handleMessageFromClient(new ObjectMessage("booooom2"));
+	}
 
-    @FXML
-    void saveAddNewBook(ActionEvent event) {
+	@FXML
+	void initialize() 
+	{
+		client=StartPanelController.connToClientController;
+		client.setClientUI(this);
 
-    }
+	}
 
-    @FXML
-    void uploadTableOfContents(ActionEvent event) {
-
-    }
-
-    @FXML
-    void initialize() {
-        
-
-    }
+	//lo asiti
+	@Override
+	public void display(ObjectMessage msg)
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
