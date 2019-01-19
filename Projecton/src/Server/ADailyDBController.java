efaa97fd60c1f565d516787527d8a9b2cadd9496
ziper02 <ziper02@gmail.com>
2019-01-20 01:20:16 +0200

    ..

--- Projecton/src/Server/ADailyDBController.java
+++ Projecton/src/Server/ADailyDBController.java
@@ -1,6 +1,22 @@
 package Server;
 
+import java.sql.Connection;
+
+import Common.ObjectMessage;
+
 public abstract class ADailyDBController 
 {
+	public static ObjectMessage selection(ObjectMessage msg, Connection connToSQL)
+	{
 
+		if (((msg.getMessage()).equals("checkIfAllBorrowed")))
+		{
+			return null;
+		}
+		else
+		{
+			return null; 
+		}
+	}
 }
+
