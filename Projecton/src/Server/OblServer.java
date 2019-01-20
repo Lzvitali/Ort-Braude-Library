327d5d88d536574aa71a5279bfb75ba8302dff79
Nataliya Kamyenyeva <Nataliya Kamyenyeva@DESKTOP-MTH3PAF>
2019-01-20 20:32:53 +0200

    open PDF from 'start panel' is working

--- Projecton/src/Server/OblServer.java
+++ Projecton/src/Server/OblServer.java
@@ -130,7 +130,7 @@ public void handleMessageFromClient(Object msg, ConnectionToClient client)
 			sock = new Socket("localhost", 5643);
 			byte[] mybytearray = new byte[Integer.parseInt(objectMessage.getMessage())];
 			InputStream is = sock.getInputStream();
-			FileOutputStream fos = new FileOutputStream("pdfFiles\\"+objectMessage.getExtra());
+			FileOutputStream fos = new FileOutputStream("pdfFiles\\"+objectMessage.getExtra() + ".pdf");
 			BufferedOutputStream bos = new BufferedOutputStream(fos);
 			int bytesRead = is.read(mybytearray,0, Integer.parseInt(objectMessage.getMessage()));
 			int current = bytesRead; 
