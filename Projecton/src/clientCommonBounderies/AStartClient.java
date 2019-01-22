73e75ec139e80aab7622746833cccdd79f68461f
Vitali <Vitali@10.0.0.5>
2019-01-22 22:21:16 +0200

    adapted all StartPanels GUIs

--- Projecton/src/clientCommonBounderies/AStartClient.java
+++ Projecton/src/clientCommonBounderies/AStartClient.java
@@ -52,7 +52,7 @@ public class AStartClient extends Application
         }
         root.setId("pane");
         primaryStage.setTitle("OBL");
-        Scene scene=new Scene(root, 1300, 774);
+        Scene scene=new Scene(root, 1400, 774);
         primaryStage.setScene(scene);
         primaryStage.setOnCloseRequest(e->{ System.exit(0);}); // if the user press "cross" button , its close all threads ( exit from process);
         primaryStage.show(); //start the scene
