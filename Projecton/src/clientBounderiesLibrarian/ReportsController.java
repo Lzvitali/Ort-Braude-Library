da2f795b88f8f82ae3835213c7036e8d852b1701
Vitali <Vitali@LAPTOP-HSFD7VG6>
2019-01-24 08:28:24 +0200

    finished Reports fxml and made ready it's controller

--- Projecton/src/clientBounderiesLibrarian/ReportsController.java
+++ Projecton/src/clientBounderiesLibrarian/ReportsController.java
@@ -1,6 +1,111 @@
 package clientBounderiesLibrarian;
 
-public class ReportsController 
+import com.jfoenix.controls.JFXTextField;
+
+import Common.IGUIController;
+import Common.ObjectMessage;
+import clientCommonBounderies.StartPanelController;
+import clientConrollers.OBLClient;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ReportsController implements IGUIController
 {
 
+	OBLClient client;
+	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField activeReaderAccounts;
+
+    @FXML
+    private TextField freezedReaderAccounts;
+
+    @FXML
+    private TextField lockedReaderAccounts;
+
+    @FXML
+    private TextField totalNumOfCopies;
+
+    @FXML
+    private TextField numOfdelayedReaderAccounts;
+
+    @FXML
+    private ComboBox<?> monthComboBox;
+
+    @FXML
+    private ComboBox<?> yearComboBox;
+
+    @FXML
+    private ComboBox<?> chooseFromPreviousComboBox;
+
+    @FXML
+    private TextField avgForRegular;
+
+    @FXML
+    private TextField medianForRegular;
+
+    @FXML
+    private BarChart<?, ?> diagramForRegular;
+
+    @FXML
+    private TextField avgForDesired;
+
+    @FXML
+    private TextField medianForDesired;
+
+    @FXML
+    private BarChart<?, ?> diagramForDesired;
+
+    @FXML
+    private TextField avgForAll;
+
+    @FXML
+    private TextField medianForAll;
+
+    @FXML
+    private BarChart<?, ?> diagramForAll;
+
+    @FXML
+    private TextField avgForNumLateReturns;
+
+    @FXML
+    private TextField medianForNumLateReturns;
+
+    @FXML
+    private BarChart<?, ?> diagramForNumLateReturns;
+
+    @FXML
+    private TextField avgForDurationLateReturns;
+
+    @FXML
+    private TextField medianForDurationLateReturns;
+
+    @FXML
+    private JFXTextField BookIDReport3;
+
+    @FXML
+    void initialize() 
+    {
+    	client=StartPanelController.connToClientController;
+    	client.setClientUI(this);
+
+    }
+
+	@Override
+	public void display(ObjectMessage msg) 
+	{
+		
+		
+	}
 }
