a92c7d927aa87a96952be3d9dbd33215dfebbb2d
Inbar <Inbar@10.0.0.12>
2019-01-24 20:22:13 +0200

    working on Report2

--- Projecton/src/Common/Report.java
+++ Projecton/src/Common/Report.java
@@ -4,5 +4,40 @@ import java.io.Serializable;
 
 public class Report implements IEntity , Serializable
 {
+	private float average;
+	private float median;
 
+
+
+	public Report(float average,float median)
+	{
+		this.average=average;
+		this.median=median;	
+	}
+
+
+
+	public float getAverage() {
+		return average;
+	}
+
+
+
+	public void setAverage(float average) {
+		this.average = average;
+	}
+
+
+
+	public float getMedian() {
+		return median;
+	}
+
+
+
+	public void setMedian(float median) {
+		this.median = median;
+	}
+	
+	
 }
